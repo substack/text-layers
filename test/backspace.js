9f05d37418488a34a0ec39062305eda49e125a7d
James Halliday <mail@substack.net>
2016-06-15 23:14:05 -0500

    failing second backspace case

--- test/backspace.js
+++ test/backspace.js
@@ -18,3 +18,12 @@ test('backspace', function (t) {
   t.equal(output, 'one\ntw_\bo abcdef\nthree')
   t.end()
 })
+
+test('backspace (2)', function (t) {
+  var output = layers([
+    { text: 'one\ntw_\booo123\nthree', x: 0, y: 0 },
+    { text: 'abc', x: 9, y: 1 }
+  ])
+  t.equal(output, 'one\ntw_\booo123 abc\nthree')
+  t.end()
+})
